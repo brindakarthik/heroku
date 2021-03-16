8422f9b0dbd777f211ba3ff5e26b070cb2a1412c
brindakarthik <79500601+brindakarthik@users.noreply.github.com>
2021-03-16 15:17:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ const cors = require('cors')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+
+  const timestamp = new Date().getTime() - 30000
+  const msg = Buffer.from("QE2SZLhBQHK_PpP25tIzfw" + req.body.meetingNumber + timestamp + req.body.role).toString('base64')
+  const hash = crypto.createHmac('sha256',"qrwzZzp1WmPJL2hfFyOV6j8SLeDI1JBCMAVf").update(msg).digest('base64')
+  const signature = Buffer.from(`${"QE2SZLhBQHK_PpP25tIzfw"}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')
+
+  res.json({
+    signature: signature
+  })
+})
+
 app.get('/', (req, res) => {
 res.send("welcome to KB")
 
 })
+
+
 
 
 var server = app.listen(process.env.PORT || 5000, function () {
