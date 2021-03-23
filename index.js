57996f1c71b674fa32d31af144dc7a85ee253936
brindakarthik <79500601+brindakarthik@users.noreply.github.com>
2021-03-23 13:28:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const crypto = require('crypto')
@@ -6,17 +8,26 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json(), cors())
 app.options('*', cors());
 
-app.use(cors());
-app.post('/', (req, res) => {
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+app.post('/', (req, res) => {
+  
+  
   const timestamp = new Date().getTime() - 30000
   const msg = Buffer.from("QE2SZLhBQHK_PpP25tIzfw" + req.body.meetingNumber + timestamp + req.body.role).toString('base64')
   const hash = crypto.createHmac('sha256',"qrwzZzp1WmPJL2hfFyOV6j8SLeDI1JBCMAVf").update(msg).digest('base64')
-  const signature = Buffer.from(`"QE2SZLhBQHK_PpP25tIzfw".${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')
+  const signature = Buffer.from(`${"QE2SZLhBQHK_PpP25tIzfw"}.${req.body.meetingNumber}.${timestamp}.${req.body.role}.${hash}`).toString('base64')
 
+
+ 
   res.json({
     signature: signature
   })
